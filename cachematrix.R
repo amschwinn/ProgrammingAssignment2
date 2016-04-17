3ab23147d468df89a43b4a244e2d4902415cc850
amschwinn <schwinnam@gmail.com>
2016-04-17 17:10:36 -0500

    Complete Homework Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these functions are to cut down on repitition of creating inverse of a matrix.
+## These will cache a matrix and its inverse so that R can just recall the cache versions for furture uses
 
-## Write a short comment describing this function
+## This function creates the cache of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  matInv  <- NULL
+  set     <- function(newMat) {
+    mat     <<- newMat
+    matInv  <<- NULL
+  }
+  get         <- function() mat
+  setInverse  <- function(inverse) matInv <- inverse
+  getInverse  <- function() matInv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+##Creates the cache inverse if there is not already one and if there is a cache, calls it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat, ...) {
+  matInv <- mat$getInverse()
+  if(!is.null(matInv)){
+    message("getting cached data")
+    return(matInv)
+  }
+  matData <- mat$get()
+  matInv  <- solve(matData, ...)
+  mat$setInverse(matInv)
+  matInv
 }
